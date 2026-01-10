7fab3c41220f5dd65e42a405af25a11ec5b4b778
test <test@test.com>
2026-01-10 15:02:49 +0000

    ok

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Hell, I am GoLang!")
+                fmt.Fprintf(w, "Hell, I am Jasur!")
         })
 
         http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
