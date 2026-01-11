c49a93ac73548fe83902ee0f413e9a64e1d2815a
test <test@test.com>
2026-01-11 08:35:49 +0000

    ok

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, "Hello, I am Golang friend!")
+                fmt.Fprintf(w, "Hello, I am Golang language!")
         })
 
         http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
